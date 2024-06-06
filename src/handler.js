895d9d0d8672317bfcd2f8bbfec20f92f05cbbc8
Mfuadshidqi <fuadshidqi23@gmail.com>
2024-06-06 07:19:00 +0700

    edit note

--- src/handler.js
+++ src/handler.js
@@ -5,7 +5,7 @@ const { nanoid } = require('nanoid');
 const addNoteHandler = (request, h) => {
     const { title, tags, body } = request.payload;
     const id = nanoid(16);
-    const createdAt = new Date().toISOString;
+    const createdAt = new Date().toISOString();
     const updatedAt = createdAt;
 
 
@@ -38,4 +38,30 @@ const addNoteHandler = (request, h) => {
 
 };
 
-module.exports = { addNoteHandler };
+const getAllNotesHandler = () => ({
+    status: 'success',
+    data: {
+      notes,
+    },
+});
+const getNoteByIdHandler = (request, h) => {
+    const { id } = request.params;
+    const note = notes.filter((n) => n.id === id)[0];
+    if (note !== undefined) {
+        return {
+          status: 'success',
+          data: {
+            note,
+          },
+        };
+      }
+     
+      const response = h.response({
+        status: 'fail',
+        message: 'Catatan tidak ditemukan',
+      });
+      response.code(404);
+      return response;
+};
+
+module.exports = { addNoteHandler, getAllNotesHandler, getNoteByIdHandler  };
